89dbbcb31c6a90f1ee48a25c962d4e19efd7f538
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-03-13 13:46:17 +0200

    create category & product models

---
+++ shop_project/shop_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
+        
