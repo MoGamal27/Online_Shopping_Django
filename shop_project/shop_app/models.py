68c899cf4a72a143fa18ab927e215fcba8c2c333
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-02 18:45:59 +0200

    Update models.py

--- shop_project/shop_app/models.py
+++ shop_project/shop_app/models.py
@@ -20,6 +20,44 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
-        
-            
+    
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+    )
+
+    products = models.ManyToManyField(Product, through="OrderItem", related_name='orders')
+
+    def __str__(self):
+        return f"Order {self.order_id } by {self.user.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_subtotal(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
+
+
         
